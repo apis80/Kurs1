eaebefcfc710467a0942d66c9c9cfef1982fef77
apis8 <apis80@mail.ru>
2022-07-21 22:25:02 +0100

    Первый комит

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,54 @@
 public class Employee {
-}
+    private String staff;
+    private int salaryStaff;
+    private int departmentNumber;
+    private int id;
+    private static int counter = 0;
+
+    public Employee(String staff, int salaryStaff, int departmentNumber) {
+        this.staff = staff;
+        this.salaryStaff = salaryStaff;
+        this.departmentNumber = departmentNumber;
+        this.id = getCounter();
+        counter ++;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public String getStaff() {
+        return staff;
+    }
+
+    public int getSalaryStaff() {
+        return salaryStaff;
+    }
+
+    public int getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setStaff(String staff) {
+        this.staff = staff;
+    }
+
+    public void setSalaryStaff(int salaryStaff) {
+        this.salaryStaff = salaryStaff;
+    }
+
+    public void setDepartmentNumber(int departmentNumber) {
+        this.departmentNumber = departmentNumber;}
+
+
+    public String toString() { return this.staff + ","+ this.salaryStaff +","+ this.departmentNumber;}
+    }
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        Employee[] employee = new Employee[10];
+        employee[0] = new Employee("Piskunov ivan Ivanovich", 73000, 1);
+        employee[1] = new Employee("Zaitsev ivan Ivanovich", 74000, 2);
+        employee[2] = new Employee("Hlystov ivan Ivanovich", 75000, 3);
+        employee[3] = new Employee("Krutov ivan Ivanovich", 79000, 4);
+        employee[4] = new Employee("Chizhov ivan Ivanovich", 67000, 5);
+        employee[5] = new Employee("Smirnov ivan Ivanovich", 55000, 1);
+        employee[6] = new Employee("Hrushov ivan Ivanovich", 98000, 2);
+        employee[7] = new Employee("Mechov ivan Ivanovich", 76000, 3);        employee[0] = new Employee("Piskunov ivan Ivanovich", 73000, 1);
+        employee[8] = new Employee("Sidorov ivan Ivanovich", 73000, 4);
+        employee[9] = new Employee("Petrov ivan Ivanovich", 73000, 5 );
+        System.out.println (Arrays.toString(employee));
+
+        private static int countMinSalary {
+            getSalaryStaff();
+
+        }
+            for (int i = 0; i < employee.length; i++) {
+
+            }
+        }
+        //countMaxSalary(employee);
+        //countStaffSalary(employee);
+        //averageSalary(employee);
+        //countAllStaff(employee);
+
+
 
     }
-}
